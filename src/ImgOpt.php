8e67a16724c4165bf79748c1d09bb61c1b9f7b24
rhladun <rec2002@ukr.net>
2024-10-10 15:45:14 +0300

    Update ImgOpt.php
    update original rep

--- src/ImgOpt.php
+++ src/ImgOpt.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @link https://www.pelock.com/
- * @copyright Copyright (c) 2021-2023 PELock LLC
+ * @copyright Copyright (c) 2024 Intelligent System Design
  * @license Apache-2.0
  */
-namespace pelock\imgopt;
+namespace rhladun\imgopt;
 
 use Yii;
 use yii\base\Widget;
@@ -26,49 +25,38 @@ use yii\helpers\Html;
  * Replace your IMG tag within your templates with a call to:
  *
  * ```php
- * <?= \pelock\imgopt\ImgOpt::widget(["src" => "/images/product/extra.png", "alt" => "Extra product" ]) ?>
+ * <?= \rhladun\imgopt\ImgOpt::widget(["src" => "/images/product/extra.png", "alt" => "Extra product" ]) ?>
  * ```
  *
  *  And it will generate a WebP & AVIF image files (original image is left untouched) and
  *  the following HTML code gets generated:
  *
- * ```html
+ * ```output html 
  * <picture>
- *     <source type="image/avif" srcset="/images/product/extra.avif">
- *     <source type="image/webp" srcset="/images/product/extra.webp">
+  *     <source type="image/webp" srcset="/images/product/webp/extra.webp">
  *     <img src="/images/product/extra.png" alt="Extra product">
  * </picture>
  * ```
- *
- * You can also generate Lightbox (https://lokeshdhakar.com/projects/lightbox2/) friendly images.
- *
- * Instead of:
- *
- * ```html
- * <a href="/images/sunset.jpg" data-lightbox="image-1" data-title="Sunset">
- *     <img src="/images/sunset-thumbnail.jpg" alt="Sunset">
- * </a>
- * ```
- *
- * You can replace it with more compact widget code:
- *
+ * Example for using for img tag with rezizes 
  * ```php
- * <?= \pelock\imgopt\ImgOpt::widget(["lightbox_data" => "image-1", "lightbox_src" => "/images/sunset.jpg', "src" => "/images/sunset-thumbnail.jpg', "alt" => "Sunset" ]) ?>
+ *   <?= \rhladun\ImgOpt\ImgOpt::widget(["src" => "/images/product/extra.png", "alt" => "Image 2", 'type_src'=>'srcset_webp', 'sizes' =>[576, 768, 992]]) ?> 
  * ```
- *
- * And it will generate this HTML code:
- *
- * ```html
- * <a href="/images/sunset.jpg" data-lightbox="image-1" data-title="Sunset">
- *     <picture>
- *         <source type="image/avif" srcset="/images/sunset-thumbnail.avif">
- *         <source type="image/webp" srcset="/images/sunset-thumbnail.webp">
- *         <img src="/images/sunset-thumbnail.png" alt="Sunset">
- *     </picture>
- * </a>
+ * 
+ * ```output html
+ *  <img src="/images/product/extra.png" srcset="/images/product/webp/4@576x414.webp 576w,/images/product/webp/extra@768x552.webp 768w,/images/product/webp/extra@992x713.webp 992w,/images/product/webp/extra.webp 10000w" alt="Image 2">  
+ * ``` 
+ * 
+ * Example for using for replace background image
+ * ```php
+ *   <div style="<?= \rhladun\ImgOpt\ImgOpt::widget(["src" => "/images/product/extra.png", 'type_src'=>'background_webp']) ?>"></div> 
  * ```
+ * 
+ * ```output html
+ *  <div class="image_block image_bg" style="background-image: url('/images/product/webp/extra.webp')"></div>  
+  * ```
  *
  * @property string $src image source relative to the @webroot Yii2 alias (required)
+ * @property string $type_src output format tags picture|srcset_webp|srcset_avif|background_webp|background_avif (required)
  * @property string $alt image alternative description used as alt="description" property (optional)
  * @property string $css image class list as a string (can contain multiple classes) used as class="one two three..." (optional)
  * @property string $style image custom CSS styles used as style="one; two; three;..." (optional)
@@ -76,13 +64,12 @@ use yii\helpers\Html;
  * @property string $itemprop use schema itemprop="image" value (optional)
  * @property string $height  height used as height="value" (optional)
  * @property string $width width used as width="value" (optional)
- * @property string $lightbox_data Lightbox attribute data-lightbox="image-1" etc. (optional)
- * @property string $lightbox_src Lightbox HREF to the original image file, if not set $src param will be used (optional)
- * @property string $lightbox_title Lightbox description title, if not set $alt param will be used (optional)
  * @property bool $recreate set to TRUE to recreate the WebP file again (optional)
  * @property bool $disable set to TRUE to disable WebP images serving (optional)
+ * @property string $_webp_path path to the stored WebP file format, (short path) or null (like "/webp") (optional)
+ * @property string $_avif_path path to the stored AVIF file format, (short path) or null (like "/avif") (optional)
+ * @property array auto resize images for specific  dimensions (array or null) (like [576, 768, 992, 1200]) (optional)
  *
- * @author Bartosz Wójcik <support@pelock.com>
  */
 class ImgOpt extends Widget
 {
@@ -92,14 +79,24 @@ class ImgOpt extends Widget
 	public $src;
 
 	/**
-	 * @var string path to the generated WebP file format (short path) or null
-	 */
-	private $_webp = null;
+	 * @var array images list (required)
+	 */	
+	private $_items = null;
 
 	/**
-	 * @var string path to the generated AVIF file format (short path) or null
+	 * @var string path to the stored WebP file format, (short path) or null (like "/webp")
 	 */
-	private $_avif = null;
+	public $_webp_path = "/webp";
+
+	/**
+	 * @var string path to the stored AVIF file format, (short path) or null (like "/avif")
+	 */
+	public $_avif_path = "/avif";
+
+	/**
+	 * @var array resize images for specific  dimensions (array or []) (like [576, 768, 992, 1200])
+	 */
+	public $sizes = [];
 
 	/**
 	 * @var string image alternative description used as alt="description" property (optional)
@@ -137,44 +134,19 @@ class ImgOpt extends Widget
 	public $width;
 
 	/**
-	 * @var string Lightbox attribute data-lightbox="image-1" etc. (optional)
-	 */
-	public $lightbox_data;
-
-	/**
-	 * @var string Lightbox HREF to the original image file, if not set $src param will be used (optional)
-	 */
-	public $lightbox_src;
-
-	/**
-	 * @var string Lightbox description title, if not set $alt param will be used (optional)
-	 */
-	public $lightbox_title;
-
-	/**
 	 * @var bool set to TRUE to recreate the WebP and AVIF files again (optional)
 	 */
 	public $recreate = false;
 
 	/**
-	 * @var bool set to TRUE to recreate *ALL* of the WebP and AVIF files again (optional)
-	 */
-	const RECREATE_ALL = false;
-
-	/**
-	 * @var bool set to TRUE to disable WebP images serving (optional)
+	 * @var bool set to TRUE to disable WebP/AVIF images serving (optional)
 	 */
 	public $disable = false;
 
 	/**
-	 * @var string disable WebP files usages at all (use it for debugging purposes) (optional)
+	 * @var string picture_webp|picture_avif|srcset_webp|srcset_avif|background_webp|background_avif 
 	 */
-	const DISABLE_WEBP = false;
-
-	/**
-	 * @var string disable AVIF files usages at all (use it for debugging purposes) (optional)
-	 */
-	const DISABLE_AVIF = false;
+	public $type_src = 'picture_webp';	
 
 	/**
 	 * Generates optimized WebP/AVIF file from the provided image, relative to the
@@ -184,15 +156,30 @@ class ImgOpt extends Widget
 	 * @param bool $recreate Recreate the output file again
 	 * @return string|null Path to the output image file (relative to @webroot) or null (marks usage of the original image only)
 	 */
-	private function get_or_convert_to_dest_format($img, $global_flag, $file_extension, $convertion_function, $recreate = false)
+	private function get_or_convert_to_dest_format($img, $recreate = false, $resize_width = null)
 	{
-		if ( ($global_flag === true) || ($this->disable == true) || (function_exists($convertion_function) == false) )
+
+		$pre_path = '';
+
+		if (in_array($this->type_src, array('picture_avif', 'srcset_avif', 'background_avif'))) {
+			$file_extension = ".avif";
+			$convertion_function = "imageavif";
+			if ($this->_avif_path) $pre_path = $this->_avif_path;
+		} else {
+			$file_extension = ".webp";
+			$convertion_function = "imagewebp";
+			if ($this->_webp_path) $pre_path = $this->_webp_path;
+		}
+
+
+		if (($this->disable == true) || (function_exists($convertion_function) == false) )
 		{
 			return null;
 		}
 
 		// build full path to the image (relative to the webroot)
 		$web_root = Yii::getAlias('@webroot');
+
 		$img_full_path = $web_root . $img;
 
 		// check if the source image exist
@@ -203,6 +190,7 @@ class ImgOpt extends Widget
 
 		// modification time of the original image
 		$img_modification_time = filemtime($img_full_path);
+
 
 		$original_file_size = filesize($img_full_path);
 
@@ -215,12 +203,26 @@ class ImgOpt extends Widget
 		$short_file_info = pathinfo($img);
 		$file_info = pathinfo($img_full_path);
 
-		$ext = strtolower($file_info["extension"]);
 
+		if (!is_dir($file_info["dirname"].$pre_path)) {
+			mkdir($file_info["dirname"].$pre_path, 0755);
+		}
+
+		$ext = strtolower($file_info["extension"]);
 		$output_filename_with_extension = $short_file_info["filename"] . $file_extension;
 
-		$output_short_path = $short_file_info["dirname"] . "/" . $output_filename_with_extension;
-		$output_full_path = $file_info["dirname"]  . "/" . $output_filename_with_extension;
+		if (!in_array($ext, array('png', 'jpg', 'jpeg'))) return null;
+
+
+		if ($resize_width) {
+			list($orig_width, $orig_height) = getimagesize($file_info["dirname"]. "/" .$file_info['basename']);
+			if ($resize_width>=$orig_width) return null;
+		 	$resize_height = ceil($orig_height*($resize_width/$orig_width));
+	 		$output_filename_with_extension = $short_file_info["filename"].'@'.$resize_width.'x'.$resize_height.$file_extension;
+		}
+
+	  	$output_short_path = $short_file_info["dirname"]. $pre_path . "/" . $output_filename_with_extension;
+	  	$output_full_path = $file_info["dirname"]. $pre_path. "/" . $output_filename_with_extension;
 
 		// if the WEBP file already exists check if we want to re-create it
 		if ($recreate === false && file_exists($output_full_path))
@@ -252,11 +254,24 @@ class ImgOpt extends Widget
 			imagepalettetotruecolor($img);
 			imagealphablending($img, true);
 			imagesavealpha($img, true);
+			if ($resize_width) {
+				$tmp = imagecreatetruecolor($resize_width, $resize_height);
+				imagecopyresampled($tmp, $img, 0, 0, 0, 0, $resize_width, $resize_height, $orig_width, $orig_height);
+				$img = $tmp;
+			}
+			
+
 		}
 		else if ($ext === "jpg" || $ext === "jpeg")
 		{
 			$img = imagecreatefromjpeg($img_full_path);
 			imagepalettetotruecolor($img);
+			if ($resize_width) {
+				$tmp = imagecreatetruecolor($resize_width, $resize_height);
+				imagecopyresampled($tmp, $img, 0, 0, 0, 0, $resize_width, $resize_height, $orig_width, $orig_height);
+				$img = $tmp;
+			}
+
 		}
 
 		// start with 100 quality
@@ -309,36 +324,82 @@ class ImgOpt extends Widget
 	public function init()
 	{
 		parent::init();
+		asort($this->sizes);
 
-		$this->_webp = $this->get_or_convert_to_dest_format($this->src, self::DISABLE_WEBP, ".webp", "imagewebp", (self::RECREATE_ALL == true || $this->recreate == true));
-		$this->_avif = $this->get_or_convert_to_dest_format($this->src, self::DISABLE_AVIF, ".avif", "imageavif", (self::RECREATE_ALL == true || $this->recreate == true));
-
-
-		// handle Lightbox parameters
-		if ($this->lightbox_data)
-		{
-			// if lightbox source image is not defined
-			// use the default image source (you might want
-			// to use thumbnail as an image BUT full res
-			// image for lightbox presentation)
-			if ($this->lightbox_src === null)
-			{
-				$this->lightbox_src = $this->src;
-			}
-
-			// same for lightbox title
-			if ($this->lightbox_title === null)
-			{
-				$this->lightbox_title = $this->alt;
-			}
+		if (is_array($this->sizes)) foreach ($this->sizes as $val) {
+			if ($tmp=$this->get_or_convert_to_dest_format($this->src, $this->recreate == true, $val)) $this->_items[$val] = $tmp;
 		}
+
+		if ($tmp = $this->get_or_convert_to_dest_format($this->src, $this->recreate == true)) $this->_items[0] = $tmp;
 	}
 
-	public function run()
-	{
-		// our unoptimized image (include all the possible attributes)
-		$img = Html::img($this->src, [
+	/**
+	 * Generates image with srcset atrribute and  resize options
+	 */
 
+	private function srcset() {
+
+		if (is_null($this->_items)) return null;
+
+		if (sizeof($this->_items)>=1) foreach ($this->_items as $key=>$item) if (!empty($item))  {
+			$srcset[] =  ($key>0) ? $item." ".$key."w" : $item." 10000w";
+		}
+
+		return $this->img(array("srcset" => implode(",", $srcset)));
+	}
+
+	/**
+	 * Generates Image inside of <picture> elements support complex adaptive source sets combining rules for width
+	 */
+
+	private function picture() {
+
+		if (is_null($this->_items)) return null;
+
+		// include it within <picture> tag
+		$html = "<picture>";
+		foreach ($this->_items as $key=>$item) if (!empty($item))  {
+			$options =  ["srcset" => $item, "type" => ($this->type_src=='picture_webp') ? "image/webp" : "image/avif"];
+			if ($key>0) $options['media'] = "(max-width:".$key."px)";
+			$html .= Html::tag("source", [], $options);
+		}
+
+		// fallback image (unoptimized)
+		$html .= $this->img();
+		$html .= "</picture>";
+
+		return $html;
+
+	}
+
+
+	private function background() {
+
+		if (is_null($this->_items)) return null;
+
+		$background_image[] =  "background-image: url('".$this->_items[0]."')";
+
+
+		if (sizeof($this->_items)>1) {
+
+			$webkit_image_set = 'background-image: -webkit-image-set(';
+			$image_set = 'background-image: image-set(';
+
+			foreach ($this->_items as $key=>$item) if (!empty($item) && $key>0)  {
+				$image_set_arr[] = "url('".$item."') ".$key."w";
+			}
+			$webkit_image_set .= implode(', ', $image_set_arr).')';
+			$image_set .= implode(',', $image_set_arr).')';
+
+			$background_image[] = $webkit_image_set;
+			$background_image[] = $image_set;
+		}
+		return implode('; ', $background_image);
+	}
+
+	private function img($attr_add=array()) {
+
+		$attr = [
 			"class" => $this->css,
 			"style" => $this->style,
 			"alt" => $this->alt,
@@ -346,34 +407,33 @@ class ImgOpt extends Widget
 			"width" => $this->width,
 			"loading" => $this->loading,
 			"itemprop" => $this->itemprop
-		]);
+		];
 
-		// was WebP image generated from our unoptimized image?
-		if ($this->_webp != null || $this->_avif != null)
-		{
-			// include it within <picture> tag
-			$html = "<picture>";
+		$attr = array_merge($attr, $attr_add);
 
-			if ($this->_avif) $html .= Html::tag("source", [], ["srcset" => $this->_avif, "type" => "image/avif"]);
-			if ($this->_webp) $html .= Html::tag("source", [], ["srcset" => $this->_webp, "type" => "image/webp"]);
+		return $img = Html::img($this->src, $attr);
+	}
 
-			// fallback image (unoptimized)
-			$html .= $img;
-			$html .= "</picture>";
 
+
+	public function run()
+	{
+		// our unoptimized image (include all the possible attributes)
+		$img = $this->img();
+		switch ($this->type_src) {
+			case 'srcset_webp':
+			case 'srcset_avif':				
+				$img = $this->srcset();
+				break;	
+			case 'picture_webp':
+			case 'picture_avif':				
+				$img = $this->picture();
+				break;
+			case 'background_webp':
+			case 'background_avif':				
+				$img = $this->background();
+				break;	
 		}
-		else
-		{
-			$html = $img;
-		}
-
-		// if lightbox attribute is present - wrap the image into a lightbox friendly
-		// <a href link
-		if ($this->lightbox_data)
-		{
-			return Html::a($html, $this->lightbox_src, [ "data-lightbox" => $this->lightbox_data, "data-title" => $this->lightbox_title ] );
-		}
-
-		return $html;
+		return $img;
 	}
 }
